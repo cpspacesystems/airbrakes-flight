3ffac1a9a64a4735ca57a9370827ed41e31ef53e
spagdoon0411 <spandan.suthar@live.com>
2024-04-24 19:21:36 -0700

    add gui support

--- setup/ros2/run_ros2.sh
+++ setup/ros2/run_ros2.sh
@@ -1,1 +1,2 @@
-docker run --platform linux/amd64 -it --rm --name ros2 ros2
+xhost +local:docker
+docker run --platform linux/amd64 -it --rm --name ros2 --pid=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro ros2
