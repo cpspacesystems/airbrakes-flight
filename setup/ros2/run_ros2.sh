5939394d53b1e9dd202087c32f3940730ff75459
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-05-11 13:01:49 -0700

    remove space to resolve merge conflict

--- setup/ros2/run_ros2.sh
+++ setup/ros2/run_ros2.sh
@@ -2,4 +2,4 @@ imagename="cpss-ros2"
 
 docker build --platform linux/amd64 -t $imagename - < setup/ros2/ROS2.Dockerfile
 xhost +local:docker
-docker run --platform linux/amd64 -it --rm --name $imagename --pid=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v $(pwd):/home/pwd $imagename 
+docker run --platform linux/amd64 -it --rm --name $imagename --pid=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v $(pwd):/home/pwd $imagename
